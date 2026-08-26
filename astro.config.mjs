dd103986901c799a80b3f9d4215163a91662301d
jackzsps <jackzsps@yahoo.com.tw>
2026-08-26 22:50:12 +0800

    修改部屬設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jackzsps.github.io',
-  base: '/muzhuSite/',
+  site: 'https://muzhusite.pages.dev',
+  base: '/',
   trailingSlash: 'always',
   integrations: [
     mdx(),
